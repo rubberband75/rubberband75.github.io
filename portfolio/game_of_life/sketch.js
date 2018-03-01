96eeb2f8aea3ede702ada32901f476e9d23f6c83
Chandler Childs <chandler@sollenaturals.com>
2018-03-01 02:31:36 -0700

    update game_of_life controlls

--- portfolio/game_of_life/sketch.js
+++ portfolio/game_of_life/sketch.js
@@ -17,12 +17,17 @@ let canvasW;
 let canvasH;
 let resolution;
 
+let running = true;
+let randomFill = 0.9;
+
+// let history = [];
+
 function setup() {
     drawNewCanvas();
 }
 
-
 function draw() {
+    
     background(0);
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
@@ -35,21 +40,27 @@ function draw() {
             stroke(8);
             strokeWeight(2);
             rect(x, y, (canvasSize / resolution) - 0, (canvasSize / resolution) - 0)
-
         }
     }
 
+    if (running) {
+        nextGrid = make2dArray(rows, cols);
 
-    nextGrid = make2dArray(rows, cols);
-
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            nextGrid[i][j] = nextState(grid, i, j)
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                nextGrid[i][j] = nextState(grid, i, j)
+            }
         }
-    }
 
-    grid = nextGrid;
-    // sleep(50);
+        grid = nextGrid;
+
+        // if(history.length < 20){
+        //     history.push(grid);
+        // } else if(history.length == 20){
+        //     history.shift()
+        //     history.push(grid);
+        // }
+    }
 }
 
 function nextState(grid, r, c) {
@@ -81,7 +92,6 @@ function nextState(grid, r, c) {
     return newState;
 }
 
-
 function drawNewCanvas() {
     let w = window.innerWidth;
     let h = window.innerHeight;
@@ -100,14 +110,41 @@ function drawNewCanvas() {
     grid = make2dArray(rows, cols);
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            let r = floor(random(2))
+            let r = int(random(1) >= randomFill)
             grid[i][j] = r;
         }
     }
 
     createCanvas(w, h);
+
+    // history = [];
 }
 
+function randomize() {
+    let runnningState = running;
+    running = true;
+    drawNewCanvas();
+    draw();
+    running = runnningState;
+}
+
+function clearGrid() {
+    running = false;
+    let oldFill = randomFill;
+    randomFill = 2;
+    randomize();
+    randomFill = oldFill;
+}
+
+function toggleGame() {
+    running = !running;
+}
+
+function stepGeneration() {
+    running = true;
+    draw();
+    running = false;
+}
 
 let slider = document.getElementById('myRange')
 resolution = slider.value
@@ -126,7 +163,62 @@ window.addEventListener("resize", function () {
     box.style.left = (window.innerWidth - box.offsetWidth) / 2;
 });
 
+document.addEventListener("keydown", function(event) {
+    // console.log(event.which);
+    switch (event.which) {
+        case 32: //space
+            toggleGame();
+            break;
+        case 39: //right arrow
+            stepGeneration();
+            break;
+        // case 37: //left arrow
+        //     if(history.length > 0){
+        //         let runnningState = running;
+        //         running = false;
+        //         grid = history.pop();
+        //         draw();
+        //         running = runnningState;
+        //     }
+        //     break;
+        case 82: //r
+            randomize();
+            break;
+        case 67: //c
+            clearGrid();
+            break;
+            
+        case 38: //up arrow
+            resolution++;
+            document.getElementById('myRange').value++;
+            drawNewCanvas();
+            break;
+        case 40: //down arrow
+            if(resolution > 1){
+                resolution--;
+                document.getElementById('myRange').value--;
+                drawNewCanvas();    
+            }
+            break;
+
+        case 109: //-
+            randomFill += 0.05;
+            break;
+
+        case 107: //+
+            randomFill -= 0.05;
+            break;
+    }
+});
+
 // document.addEventListener("mousemove", function(e) {
 //     if(e.clientY < 120) {
 //     }
 // });
+
+document.addEventListener("mousedown", function(e) {
+    let cellWidth = canvasSize / resolution;
+    let ccol = floor(e.clientX/cellWidth)
+    let crow = floor(e.clientY/cellWidth)
+    grid[crow][ccol] = int(!grid[crow][ccol])
+});
